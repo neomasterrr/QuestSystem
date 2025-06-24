9596455705124e879cebe45f5166b90b7b31795f
Andrii Gastello <concof.zp@gmail.com>
2025-06-24 07:50:08 -0400

    Adding Timer and IMission

---
+++ Assets/Scripts/Interfaces/IMission.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Interfaces
+{
+    public interface IMission
+    {
+        event Action OnStarted;
+        event Action OnMissionPointReached;
+        event Action OnFinished;
+        void Start();
+    }
+
+}
