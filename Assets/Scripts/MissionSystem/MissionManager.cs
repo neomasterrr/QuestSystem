0d05e0b236c9b667a6ae131483f531fd995a7314
Andrii Gastello <concof.zp@gmail.com>
2025-06-25 01:30:13 -0400

    Minor UI enhancements + refactor

--- Assets/Scripts/MissionSystem/MissionManager.cs
+++ Assets/Scripts/MissionSystem/MissionManager.cs
@@ -16,6 +16,7 @@ namespace MissionSystem
 
         /* Private variables*/
         private List<MissionChainSO> _activeChains;
+        private List<MissionChainSO> _completedChains;
         private int _chainIndex = 0;
         
         private void Awake()
@@ -35,6 +36,7 @@ namespace MissionSystem
                 Debug.LogWarning("UIHandler not found");
             
             _activeChains = new List<MissionChainSO>();
+            _completedChains = new List<MissionChainSO>();
         }
 
         public void StartNewChain()
@@ -45,20 +47,24 @@ namespace MissionSystem
             var chain = chainQueue[_chainIndex];
             Debug.Log($"Current Chain: {chain}");
 
-            if (!_activeChains.Contains(chain))
+            if (!_activeChains.Contains(chain) && !_completedChains.Contains(chain))
             {
                 _activeChains.Add(chain);
                 
                 Action OnChainFinishedHandler = null;
-                OnChainFinishedHandler = new Action(() =>
+                OnChainFinishedHandler = () =>
                 {
                     UIHandler.SetChainName($"{chain.chainName} completed!");
 
                     // Removing a chain from the activeChains list.
                     _activeChains.Remove(chain);
+                    _completedChains.Add(chain);
                     
                     chain.OnChainFinished -= () => OnChainFinishedHandler();
-                });
+                    
+                    DrawChains();
+                    DrawMissions();
+                };
 
                 chain.OnChainFinished += () => OnChainFinishedHandler();
                 
@@ -66,44 +72,40 @@ namespace MissionSystem
             }
 
 
+            DrawChains();
+            DrawMissions();
+
+            _chainIndex++;
+        }
+
+        private void DrawChains()
+        {
             var chainsList = _activeChains
                 .Aggregate("", (current, activeChains) =>
                     current + (activeChains.chainName.ToString() + "\n"));
 
             UIHandler.SetChainName(chainsList);
-            
+        }
+
+        private void DrawMissions()
+        {
             var missionsList = _activeChains
                 .Aggregate("", (current, activeChains) =>
                     current + (activeChains.GetCurrentMission().missionName + "  " + 
                                activeChains.GetCurrentMission().GetProgress() + "/" + 
                                activeChains.GetCurrentMission().GetGoal() + "\n"));
-            
-            UIHandler.SetMissionName(missionsList);
 
-            _chainIndex++;
-            
-            /*foreach (var chains in _activeChains)
-            {
-                var mission = chains.GetCurrentMission();
-                
-                var missionsList = _activeChains
-                    .Aggregate("", (current, activeChains) =>
-                        current + (mission.missionName + "  " + 
-                                   mission.GetProgress() + "/" + 
-                                   mission.GetGoal() + "\n"));
-                
-                StartCoroutine(mission.DrawDelay(UIHandler.SetMissionName(missionsList)));
-            }*/
+            UIHandler.SetMissionName(missionsList);
         }
 
         public void AddProgress()
         {
-            foreach (var chain in _activeChains)
+            foreach (var chain in _activeChains.ToList())
             {
                 var mission = chain.GetCurrentMission();
                 mission.AddCount();
             
-                UIHandler.SetMissionName(mission.missionName + "  " + mission.GetProgress() + "/" + mission.GetGoal());
+                DrawMissions();
             }
         }
     }
